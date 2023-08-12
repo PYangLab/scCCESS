1a1fd3225b9d5ec1e5089dd8310a69116eb45100
Lijia Yu <yulj2010@gmail.com>
2023-08-12 22:48:07 +1000

    bump version and delete clusterCrit package

--- R/scCCESS.R
+++ R/scCCESS.R
@@ -265,7 +265,7 @@ ensemble_cluster = function(dat, seed = 1, cluster_func = function(x) kmeans(x, 
 #'
 #' @param x  clusters A
 #' @param y clusters B
-#' @param criteria measures include ARI, NMI, FM, and Jaccard index
+#' @param criteria measures include ARI, NMI
 #'
 #' @return the similarity value of two clusters.
 #'
@@ -275,14 +275,14 @@ ensemble_cluster = function(dat, seed = 1, cluster_func = function(x) kmeans(x, 
 #'
 #' @keywords internal
 #'
-#' @import aricode clusterCrit
+#' @import aricode
 #'
 criteria_compare = function(x, y, criteria) {
   res = switch(criteria,
                "ARI" = round(ARI(x, y), digits = 5),
-               "NMI" = round(NMI(x, y), digits = 5),
-               "FM" =  round(extCriteria(x, y, "Folkes_Mallows")$folkes_mallows, digits = 5),
-               "Jaccard" =  round(extCriteria(x, y, "Jaccard")$jaccard, digits = 5)
+               "NMI" = round(NMI(x, y), digits = 5)#,
+               # "FM" =  round(clusterCrit::extCriteria(x, y, "Folkes_Mallows")$folkes_mallows, digits = 5),
+               # "Jaccard" =  round(clusterCrit::extCriteria(x, y, "Jaccard")$jaccard, digits = 5)
   )
 
   return(res)
